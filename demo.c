417e12597ec1f205777a7bd17773f8fe60b62a4d
gulyawar157 <gulyawar.aman@intimetec.com>
2025-08-14 08:22:17 +0530

    Added new files

--- demo.c
+++ demo.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     printf("Hello World..........\n");
-int p ;
-     char ch;
     
-   printf("Enter the value of a : ");
-   scanf("%d", &p);
-   printf("Enter the value of ch : ");
-   scanf(" %c", &ch);
-
-     printf("\n The value of a and ch is : %d and %c", p, ch );
 
     return 0;
 }
